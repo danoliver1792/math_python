e603a163cc36b904d62c126e60d6cc65b45d7549
danoliver1792 <danrleioliveira1792@gmail.com>
2022-05-29 21:45:53 -0300

    Create par_or_impar.py

---
+++ fundamental/par_or_impar.py
@@ -0,0 +1,12 @@
+from random import randint
+
+print('\033[4;31mVamos ver se o número digitado é par ou ímpar? \033[m')
+
+numero = int(input('Digite um número: '))
+resultado = numero % 2
+pc = randint(0, 5)
+
+if resultado == 0:
+    print('O número {} é par'.format(numero))
+else:
+    print('O número {} é ímpar'.format(numero))
